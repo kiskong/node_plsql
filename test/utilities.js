ed746744c85ed52328e68b7abff8c3f20c35cda6
Dieter Oberkofler <dieter.oberkofler@gmail.com>
2014-06-02 10:16:35 +0200

    Fixed a few warnings

---
+++ test/utilities.js
@@ -0,0 +1,66 @@
+/**
+ * @fileoverview Test for the module "utilities.js"
+ * @author doberkofler
+ */
+
+
+/* jshint node: true */
+/* global describe: false, it:false */
+
+
+/**
+* Module dependencies.
+*/
+
+var assert = require('chai').assert;
+var utilities = require('../lib/utilities');
+
+var path = require('path');
+var fs = require('fs');
+
+
+/**
+* Module constants.
+*/
+
+
+/**
+* Module variables.
+*/
+
+
+/**
+* Tests.
+*/
+describe('absoluteFilename', function () {
+	'use strict';
+
+	describe('when getting the absolute filename', function () {
+		var FILENAME = '../test.file';
+
+		it('does return it', function () {
+			assert.strictEqual(utilities.absoluteFilename(FILENAME), path.resolve(path.join(__dirname, FILENAME)));
+		});
+	});
+
+});
+
+describe('fileCopy', function () {
+	'use strict';
+
+	describe('when copying an existing file', function () {
+		var SRC = 'test.old',
+			DST = 'test.new';
+
+		utilities.fileDelete(SRC);
+		utilities.fileDelete(DST);
+		fs.writeFileSync(SRC, 'content');
+
+		it('does copy the file', function () {
+			utilities.fileCopy(SRC, DST);
+
+			assert.strictEqual(fs.existsSync(DST), true);
+		});
+	});
+
+});
