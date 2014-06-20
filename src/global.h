d44606c2713ffdf232a2e8c3dbd1673290b5ef51
Dieter Oberkofler <dieter.oberkofler@gmail.com>
2014-06-20 18:27:16 +0200

    Missing header on linux

--- src/global.h
+++ src/global.h
@@ -9,6 +9,7 @@
 
 ///////////////////////////////////////////////////////////////////////////
 #include <assert.h>
+#include <stdlib.h>
 #include <string.h>
 #if !defined(__APPLE__)
 # include <malloc.h>
